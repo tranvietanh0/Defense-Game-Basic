92ec1f91222cfe1b0000fc88f37387b02694d8aa
Trần Việt Anh <tranvietanh0507@gmail.com>
2024-04-11 23:35:54 +0700

    Create Enemy Weapon
    Create Hero die when enemy's weapon attack

---
+++ Assets/_VANH/DefenseGameBasic/_Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VANH.DefenseBasic
+{
+    public class Enemy : MonoBehaviour, IComponentChecking
+    {
+        [SerializeField] private float m_speed;
+        [SerializeField] private float atkDistance;
+        private Animator m_anim;
+        private Rigidbody2D m_rb;
+        private Player m_player;
+        
+        private void Awake()
+        {
+            m_anim = GetComponent<Animator>();
+            m_rb = GetComponent<Rigidbody2D>();
+            m_player = FindObjectOfType<Player>();
+        }
+        public bool IsComponentsNull()
+        {
+            return m_anim == null || m_rb == null || m_player == null;
+        }
+        void Update()
+        {
+            if (IsComponentsNull())
+            {
+                return;
+            }
+
+            if (Vector2.Distance(m_player.transform.position, transform.position) <= atkDistance)
+            {
+                m_anim.SetBool(Const.ATTACK_ANIM, true);
+                m_rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                m_rb.velocity = new Vector2(-m_speed, m_rb.velocity.y);
+            }
+        }
+    }
+}
--- Assets/_VANH/DefenseGameBasic/_Scripts/Player.cs
+++ Assets/_VANH/DefenseGameBasic/_Scripts/Player.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 
 namespace VANH.DefenseBasic
 {
-    public class Player : MonoBehaviour
+    public class Player : MonoBehaviour, IComponentChecking
     {
         [SerializeField] private float m_atkRate;
         private Animator m_anim;
         private float m_curAtkRate;
         private bool m_isAttacked;
-
+        private bool m_isDeath;
         private void Awake()
         {
             m_anim = GetComponent<Animator>();
             m_curAtkRate = m_atkRate;
         }
 
+        public bool IsComponentsNull()
+        {
+            return m_anim == null;
+        }
         private void Update()
         {
+            if (IsComponentsNull())
+            {
+                return;
+            }
             if (Input.GetMouseButtonDown(0) && !m_isAttacked)
             {
-                if (m_anim)
-                {
-                    m_anim.SetBool(Const.ATTACK_ANIM, true);
-                    m_isAttacked = true;
-                }
+                m_anim.SetBool(Const.ATTACK_ANIM, true);
+                m_isAttacked = true;
             }
 
             if (m_isAttacked)
@@ -43,9 +48,25 @@ namespace VANH.DefenseBasic
 
         public void ResetAtkAnim()
         {
-            if (m_anim)
+            if (IsComponentsNull())
             {
-                m_anim.SetBool(Const.ATTACK_ANIM, false);
+                return;
+            }
+            m_anim.SetBool(Const.ATTACK_ANIM, false);
+            
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (IsComponentsNull())
+            {
+                return;
+            }
+            if (col.CompareTag("EnemyWeapon") && !m_isDeath)
+            {
+                m_anim.SetTrigger(Const.DEAD_ANIM);
+                Debug.Log(m_isDeath);
+                m_isDeath = true;
             }
         }
     }
